5c8359d2237e6864fc81aea8016c9f17d098c230
LAB13_USER ON WS18 <L13X16W12@LabHCI.net>
2026-09-16 16:36:36 +0800

    Merge branch 'feature/holiday-sale' and resolve pricing conflicts

--- pricing.js
+++ pricing.js
@@ -1,14 +1,16 @@
 function calculateTotal(items, discountCode) {
-  let subtotal = items.reduce((sum, item) => sum + item.price * item.qty, 0);
-  let discount = 0;
-
-  if (discountCode === 'SAVE10') {
-    discount = subtotal * 0.10;
-  } else if (discountCode === 'BULK20' && items.length > 5) {
-    discount = subtotal * 0.20;
-  }
-
-  return subtotal - discount;
+    let subtotal = items.reduce((sum, item) => sum + item.price * item.qty, 0);
+    let discount = 0;
+ 
+    if (discountCode === 'SAVE10') {
+        discount = subtotal * 0.10;
+    } else if (discountCode === 'HOLIDAY25') {
+        discount = subtotal * 0.25;
+    } else if (discountCode === 'BULK20' && items.length >= 5) {
+        discount = subtotal * 0.20;
+    }
+ 
+    return subtotal - discount;
 }
-
+ 
 module.exports = { calculateTotal };
